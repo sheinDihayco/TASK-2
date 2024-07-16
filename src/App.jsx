6c99a59f073e3951604ad3d46db4be1c6eaf7584
sheinDihayco <shiendihayco16@gmail.com>
2024-07-16 20:48:26 +0800

    editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import Student from "./component/Student";
-import "./Student.css" 
-  
+import "./Student.css"
+
 function App() {
   const studentRecords = [
-    { name: "Sheinalie Dihayco", department: "BSIT", fGrade: "1.5", status: true },
-    { name: "Louie Tahanlangit", department: "BSIT", fGrade: "1.1", status: true },
-    { name: "Ashielamae Aguipo", department: "BSIT", fGrade: "1.2", status: true },
-    { name: "Alaiza Jean Natad", department: "BSIT", fGrade: "1.5", status: true },
-    { name: "Zean Algarme", department: "BSIT", fGrade: "1.5", status: true },
-    { name: "Junrih Rizada", department: "BSIT", fGrade: "1.4", status: true },
+    { name: "Sheinalie Dihayco", department: "Science", fGrade: "90", status: true },
+    { name: "Louie Tahanlangit", department: "Commerce", fGrade: "90", status: true },
+    { name: "Ashielamae Aguipo", department: "Arts", fGrade: "80", status: true },
+    { name: "Alaiza Jean Natad", department: "Science", fGrade: "59", status: false },
+    { name: "Zean Algarme", department: "Commerce", fGrade: "85", status: true },
+    { name: "Junrih Rizada", department: "Science", fGrade: "99", status: true },
+    { name: "Kerwin Tapic", department: "Commerce", fGrade: "96", status: true },
+    { name: "Keneth Denampo", department: "Arts", fGrade: "82", status: true },
+    { name: "Ritch Birao", department: "Science", fGrade: "80", status: true },
+     { name: "Juan Cruz", department: "Arts", fGrade: "40", status: false },
   ];
 
   return (
     <div className="App">
-      <h1>Student Records</h1>
       <Student studentRecords={studentRecords} />
     </div>
   );
--- src/component/Student.jsx
+++ src/component/Student.jsx
@@ -2,16 +2,30 @@ import React from "react";
 
 export default function Student({ studentRecords }) {
   return (
-      <div className="App">
-          <div>  
-            <ol>
-                {studentRecords.map((studRecs) => (
-                <li key={studRecs}>
-                    {studRecs.name} {studRecs.department} {studRecs.fGrade} {studRecs.status ? "PASSED" : "FAILED"}
-                </li>
-                ))}
-            </ol>
-        </div>
+    <div className="App">
+      <h1>Student Records</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Student Names</th>
+            <th>Department</th>
+            <th>Final Grade</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {studentRecords.map((student, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{student.name}</td>
+              <td>{student.department}</td>
+              <td>{student.status ? student.fGrade : "N/A"}</td>
+              <td>{student.status ? "Active" : "Fail"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
